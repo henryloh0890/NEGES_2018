2e3454396e64b37446378e70694e2d7b09102e13
henryloh0890 <henry.loharja@gmail.com>
2022-03-24 12:46:39 +0100

    Delete corpus_SFU_Review_SP_NEG_task2/Model directory

--- corpus_SFU_Review_SP_NEG_task2/Model/remdis.py
+++
@@ -1,7 +0,0 @@
-with open("diann_esp_train.txt") as x:
-	text = x.read()
-	text = text.replace("<dis>","")
-	text = text.replace("</dis>","")
-	
-with open("diann_esp_train.txt", "w") as f:
-	f.write(text)
